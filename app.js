0a9f4adfe4ac39e1af0061b56fd30ba708662a01
pacome <pacomeberu@machine.local>
2013-12-18 16:29:10 +0100

    first push

---
+++ app.js
@@ -0,0 +1,76 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+var ArticleProvider = require('./articleprovider-mongodb').ArticleProvider;
+var routes = require('./routes');
+var user = require('./routes/user');
+var http = require('http');
+var path = require('path');
+
+var app = express();
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+app.use(express.cookieParser('your secret here'));
+app.use(express.session());
+app.use(app.router);
+app.use(require('less-middleware')({ src: path.join(__dirname, 'public') }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+// blog articles in db
+var articleProvider= new ArticleProvider('localhost', 27017);
+
+// Routes
+app.get('/', function(req, res){
+    articleProvider.findAll( function(error,docs){
+        res.render('index.ejs', { locals: {
+            title: 'Blog',
+            articles:docs
+            }
+        });
+    })
+});
+
+
+// create new post
+app.get('/blog/new', function(req, res) {
+    res.render('blog_new.ejs', { locals: {
+        title: 'New Post'
+    }
+    });
+});
+
+app.post('/blog/new', function(req, res){
+    articleProvider.save({
+        title: req.param('title'),
+        body: req.param('body')
+    }, function( error, docs) {
+        res.redirect('/')
+    });
+});
+
+
+
+
+app.get('/', routes.index);
+app.get('/users', user.list);
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
+
